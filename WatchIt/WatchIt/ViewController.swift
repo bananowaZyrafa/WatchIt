4eec14820240d592d964a94c103b1c6ab5df8163
bananowaZyrafa <wally93@gmail.com>
2016-10-20 21:29:09 +0200

    finally rxcomponents work

--- WatchIt/WatchIt/ViewController.swift
+++ WatchIt/WatchIt/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
+import Moya
+
+class Repository {
+    
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let searchResults = searchBar.rx.text
+            .throttle(0.3, scheduler: MainScheduler.instance)
+            .distinctUntilChanged()
+            .do(onNext: { (query) in
+                print(query)
+                }, onError: { (error) in
+                    print("error : \(error)")
+                }, onCompleted: nil, onSubscribe: nil, onDispose: nil)
+            .observeOn(MainScheduler.instance)
         // Do any additional setup after loading the view, typically from a nib.
+        searchResults.subscribe { (query) in
+            print(query)
+        }
     }
 
     override func didReceiveMemoryWarning() {
